2b6a88163b47174d131042d9c5fa02a6b7af0e9d
lillywho <lillywho@outlook.de>
2024-03-07 17:19:04 +0100

    Finish movement routines, add delay for button presses and throttle registration

--- train_dabble.ino
+++ train_dabble.ino
@@ -13,13 +13,26 @@ static int mode = 0; // mode 0 = absolute analogue, mode 1 = real analogue throt
 static float directionPlus = 0;
 static float directionMinus = 0;
 static float direction = 0;
-static float speed = 0;
+static int speed = 0;
+static long interval = 0;
 static long lastPuff = 0;
 static long lastPuffOff = 0;
 static bool doPuff = false;
 static bool lightsOn = false;
 static bool emergencyBrake = false;
 static bool steamerEnabled = false;
+static long lastTime = 0;
+static long deltaTime = 0;
+
+static int CircleDebounce = 0;
+static int SquareDebounce = 0;
+static int TriangleDebounce = 0;
+static int XDebounce = 0;
+static int StartDebounce = 0;
+
+static long throttleTick = 0;
+
+
 void setup() {
 
   pinMode(Motor1, OUTPUT);
@@ -36,73 +49,102 @@ void loop() {
   direction = GamePad.getYaxisData();
   directionPlus = constrain(direction,0.0,7);
   directionMinus = constrain(direction,-7.0,0);
-  speed = constrain(speed,0,maxSpeed);
-  modeUp(GamePad.isTrianglePressed());
-  modeDown(GamePad.isSquarePressed());
-  steamer(steamerEnabled);
-  if (GamePad.isCrossPressed())
-  {
-    emergencyBrake = !emergencyBrake; // toggle emergency brake
-  }
+  mode0(direction, mode == 0);
+  mode1(mode == 1);
+  mode2(mode == 2);
+  diesel(steamerEnabled);
+  digitalWrite(ledPin1,lightsOn);
 
-  if (GamePad.isCirclePressed())
-  {
-    lightsOn = !lightsOn;
-  }
-  if (GamePad.isStartPressed()) {
-    steamerEnabled = !steamerEnabled;
-  }
-
-  if (lightsOn) {
-    digitalWrite(ledPin1,ON);
-  } else {
-    digitalWrite(ledPin1,OFF);
-  }
+  //Serial.print(interval);
+  Serial.println(speed);
+  //Serial.println(mode);
   digitalWrite(13,doPuff); //just represent it via the internal LED for now
+
+  Circle(GamePad.isCirclePressed());
+  Square(GamePad.isSquarePressed());
+  Triangle(GamePad.isTrianglePressed());
+  X(GamePad.isCrossPressed());
+  Start(GamePad.isStartPressed());
   
 }
-void mode0(float value) {
-  if (!emergencyBrake) {
-    if (directionPlus > 0) {
-      speed = map(value,0, 1.0, 0 , maxSpeed); // map to the axis value if the speed isn't on emergency brake
-      digitalWrite(in1, HIGH);
-      digitalWrite(in2, LOW);
-    }
-    else {
-      speed = map(value,-1, 0, 0 , maxSpeed); // map to the axis value if the speed isn't on emergency brake
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, HIGH);
-    }
-  } else {
-    speed = 0;
+
+void Circle(bool pressed) {
+  if (!pressed) {CircleDebounce = 0; return 0;}
+  if (CircleDebounce == 0) {CircleDebounce = millis(); lightsOn == !lightsOn;}
+}
+void Square(bool pressed) {
+  if (!pressed) {SquareDebounce = 0; return 0;}
+  if (SquareDebounce == 0) {SquareDebounce = millis(); modeDown();}
+}
+void Triangle(bool pressed) {
+  if (!pressed) {TriangleDebounce = 0; return 0;}
+  if (TriangleDebounce == 0) {TriangleDebounce = millis(); modeUp();}
+}
+void X(bool pressed) {
+  if (!pressed) {XDebounce = 0; return 0;}
+  if (XDebounce == 0) {XDebounce = millis(); emergencyBrake = !emergencyBrake;}
+}
+void Start(bool pressed) {
+  if (!pressed) {StartDebounce = 0; return 0;}
+  if (StartDebounce == 0) {StartDebounce = millis(); steamerEnabled = !steamerEnabled;}
+}
+
+void mode0(float value, bool mode0) {
+  if (mode != 0) {return 0;}
+  if (emergencyBrake) {speed = 0;analogWrite(Motor1, speed); return;}
+  if (directionPlus > 0) {
+    speed = lerp(value,0,7,0,maxSpeed); // map to the axis value if the speed isn't on emergency brake
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+  }
+  else {
+    speed = lerp(value,0,7,0,maxSpeed); // map to the axis value if the speed isn't on emergency brake
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
   }
   analogWrite(Motor1, speed);
 }
-void mode1() {
-  speed = speed + direction;
-  analogWrite(Motor1, speed);
+void mode1(bool mode1) {
+  if (!mode1) {return 0;} //quit if we're not in mode 1
+  if (emergencyBrake) {speed = 0;analogWrite(Motor1, speed); return 0;} //just brake if we hit the panic button
+  if (speed == maxSpeed || speed == (maxSpeed * (-1))) {throttleTick = 0; analogWrite(Motor1, speed); return 0;} //reset tick variable, write to motor and exit
+  if (speed > 0) {digitalWrite(in1,HIGH);digitalWrite(in2,LOW);} else if (speed < 0) {digitalWrite(in1,LOW); digitalWrite(in2,HIGH);}
+  if (direction == 0) {
+    throttleTick = 0;
+    speed = speed + direction;
+  } else if (direction != 0 && throttleTick == 0) {
+    throttleTick = millis();
+    speed = speed + direction;
+  } else if (direction != 0 && millis() - throttleTick > 1000) {
+    throttleTick = millis();
+    speed = speed + direction;
+  }
+  analogWrite(Motor1, abs(speed));
 }
-void mode2() {
-  if (GamePad.isUpPressed()) {
+void mode2(bool mode2) {
+  if (!mode2) {return 0;}
+  if (emergencyBrake) {speed = 0; analogWrite(Motor1, speed); return;}
+  if (GamePad.isUpPressed() && speed < maxSpeed) {
     speed = speed + 2;
-  } else if (GamePad.isDownPressed()) {
+  } else if (GamePad.isDownPressed() && speed > 0) {
     speed = speed - 2;
   }
-  analogWrite(Motor1, speed);
+  analogWrite(Motor1, abs(speed));
+  if (speed > 0) {digitalWrite(in1,HIGH);digitalWrite(in2,LOW);} else if (speed < 0) {digitalWrite(in1,LOW); digitalWrite(in2,HIGH);}
 }
-void modeUp(bool pressed) {
-  if (!pressed) return 0;
+void modeUp() {
   if (mode == 2) return 0;
   mode = mode + 1;
 }
-void modeDown(bool pressed) {
-  if (!pressed) return 0;
+void modeDown() {
   if (mode == 0) return 0;
   mode = mode - 1;
 }
 void steamer(bool steam) {
-  if (!steam) {doPuff = false; lastPuff = 0; lastPuffOff = 0; return 0;}
-   if (!doPuff && millis() - lastPuffOff > 5000) {
+  if (!steam && smokeMode == 1) {doPuff = false; lastPuff = 0; lastPuffOff = 0; return 0;}
+  if (!steam && smokeMode != 1) {return 0;}
+///////////////////////////////////////////////////////////////////////////
+  if (!doPuff && millis() - lastPuffOff > 5000) {
     doPuff = true;
     lastPuff = millis();
   }
@@ -110,5 +152,39 @@ void steamer(bool steam) {
     doPuff = false;
     lastPuffOff = millis();
   }
-  //Serial.print(doPuff);
+}
+void diesel(bool steam) {
+  if (!steam && smokeMode == 0) {doPuff = false; lastPuff = 0; lastPuffOff = 0; return 0;}
+  if (!steam && smokeMode != 0) {return 0;}
+  interval = map(speed,0,maxSpeed,2,5);
+  if (speed == 0) { //idle smoke behaviour
+    if (!doPuff && millis() - lastPuffOff > 800) {
+    doPuff = true;
+    lastPuff = millis();
+    }
+    if (doPuff && millis() - lastPuff > 800) {
+      doPuff = false;
+      lastPuffOff = millis();
+    }
+  } else if (speed > 0) {
+    if (!doPuff && millis() - lastPuffOff > 1000 + (interval * -1)) {
+      doPuff = true;
+      lastPuff = millis();
+    }
+    if (doPuff && millis() - lastPuff > 1000 + (interval * -1)) {
+      doPuff = false;
+      lastPuffOff = millis();
+    }
+  }
+}
+
+float lerp(float x,float x0, float x1, float y0, float y1) {
+    float d = x1 - x0;
+    if (d == 0) {
+        return (y0 + y1) / 2;
+    }
+    return y0 + (x - x0) * (y1 - y0) / d;
+}
+int abs(int x) {
+    return (x < 0) ? -x : x;
 }
